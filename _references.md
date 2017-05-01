d12236bd9ebd7f6748bb80bb5e282f9874f2a506
Alison Loney <ALoney@walmartlabs.com>
2017-05-01 11:42:43 -0400

    updated and removed files

--- _references.md
+++
@@ -1,2 +0,0 @@
-# This file is needed by Java Jekyll/Idiom but not used at the moment.
-
--- promote.sh
+++ promote.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 dir=`pwd`
-name=reference.walmart.com
+name=concord.prod.walmart.com
 site="$dir/_site/"
 target=/home/jvanzyl/webserver-0.0.7-SNAPSHOT/server/sites/${name}
 rsync -avz --delete ${site} jvanzyl@devtools.walmart.com:${target}
